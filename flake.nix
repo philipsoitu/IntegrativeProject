7d6a489db3367ca46e206ebdc114a09febc94874
philipsoitu <philip.soitu@gmail.com>
2025-09-08 22:53:10 -0400

    flakes init

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Java Maven project with OpenJDK 21 dev shell";
+  description = "JavaFX Maven project with OpenJDK 21 dev shell";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -12,14 +12,44 @@
         pkgs = import nixpkgs { inherit system; };
       in {
         devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.openjdk21
-            pkgs.maven
+          packages = with pkgs; [
+            openjdk21
+            maven
+            openjfx21
+            xorg.libXxf86vm
+            xorg.libXtst  # Add this for libXtst.so.6
+            glib
+            gtk3
+            libglvnd
+            mesa
+            libthai
+            cairo
+            pango
+            gdk-pixbuf
+            atk
+            wayland
           ];
 
           shellHook = ''
-            echo "Welcome to the Java dev shell (OpenJDK 21 + Maven)"
-            echo "Run: mvn clean install"
+            # Set library paths for JavaFX native dependencies
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+              pkgs.xorg.libXxf86vm
+              pkgs.xorg.libXtst  # Add this
+              pkgs.glib
+              pkgs.gtk3
+              pkgs.libglvnd
+              pkgs.mesa
+              pkgs.libthai
+              pkgs.cairo
+              pkgs.pango
+              pkgs.gdk-pixbuf
+              pkgs.atk
+              pkgs.wayland
+            ]}:$LD_LIBRARY_PATH
+            
+            # Hint for JavaFX to use GTK3
+            export JAVA_OPTS="-Djavafx.platform=gtk"
+            echo "JavaFX dev shell ready. Run with: mvn clean javafx:run"
           '';
         };
       });
